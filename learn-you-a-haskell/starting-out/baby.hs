89ffaba171bf9c2e3ec306e1e8b455803611e5a2
Innaky <innaky@protonmail.com>
2020-01-30 17:30:25 -0400

    Some functions with definition "spec"... small examples only for practice.

--- learn-you-a-haskell/starting-out/baby.hs
+++ learn-you-a-haskell/starting-out/baby.hs
@@ -6,3 +6,10 @@ doubleUS x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
                          then x
                       else doubleMe x
+
+onlylowercase :: [Char] -> [Char] --not necesary, the compiler can infer by itself
+onlylowercase xs = [x | x <- xs, x `elem` ['a'..'z']]
+
+fact :: Integer -> Integer
+fact 0 = 1
+fact x = x * fact(x-1)
