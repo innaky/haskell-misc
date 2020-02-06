964df82c8e85dffa61e4ac4e3dec1f032b0185b0
Innaky <innaky@protonmail.com>
2020-02-06 09:02:14 -0400

    more YesNo instances and typedatas

--- learn-you-a-haskell/type_and_typeclasses/my_class.hs
+++ learn-you-a-haskell/type_and_typeclasses/my_class.hs
@@ -1,3 +1,6 @@
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Red, Eq)
+data TrafficLight = Red | Green | Yellow deriving (Show)
+
 class YesNo a where
   yesno :: a -> Bool
   
@@ -11,4 +14,15 @@ instance YesNo [a] where
   
 instance YesNo (Tree a) where
   yesno EmptyTree = False
+  yesno _ = True
+
+instance YesNo Bool where
+  yesno = id -- id is just a std lib function that takes a parameter and return the same thing
+  
+instance YesNo (Maybe a) where
+  yesno (Just _) = True
+  yesno Nothing = False
+  
+instance YesNo TrafficLight where
+  yesno Red = False
   yesno _ = True
