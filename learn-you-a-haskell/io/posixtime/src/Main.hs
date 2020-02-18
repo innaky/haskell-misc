f1367a52441a5207945fe373967dfa71e57fc630
Innaky <innaky@protonmail.com>
2020-02-17 22:05:47 -0400

    Very more functions and more readable output.

--- learn-you-a-haskell/io/posixtime/src/Main.hs
+++ learn-you-a-haskell/io/posixtime/src/Main.hs
@@ -1,34 +1,69 @@
 module Main where
 
 import System.Posix.Files
-import System.Posix.Types
+import System.FilePath.Posix (takeFileName)
 import System.Environment
 import Data.Time.Clock.POSIX
 import Data.Time.Clock
+import System.FilePath
+import System.Directory (listDirectory, doesDirectoryExist)
+import Control.Monad
+
+listDirCompletePathR :: FilePath -> IO [FilePath]
+listDirCompletePathR mainDir = do
+  lastFileNames <- listDirectory mainDir
+  paths <- forM lastFileNames $ \smallName -> do
+    let path = mainDir </> smallName
+    isDirectory <- doesDirectoryExist path
+    if isDirectory
+      then listDirCompletePathR path
+      else return [path]
+  return (concat paths)
+
+completePath :: FilePath -> IO [FilePath]
+completePath mainDir = do
+  simpleFileNames <- listDirectory mainDir
+  allPaths <- forM simpleFileNames $ \simpleName -> do
+    let path = mainDir </> simpleName
+    return [path]
+  return (concat allPaths)
 
 getAccessTime :: FilePath -> IO (POSIXTime)
 getAccessTime filePath =
   do stat <- getFileStatus filePath
      return (accessTimeHiRes stat)
 
-getModificationTime :: FilePath -> IO (POSIXTime)
-getModificationTime filePath =
-  do stat <- getFileStatus filePath
-     return (modificationTimeHiRes stat)
+getAccessTimeR :: FilePath -> IO [(FilePath, UTCTime)]
+getAccessTimeR topPath = do
+  allFiles <- listDirCompletePathR topPath
+  alloutput <- forM allFiles $ \filename -> do
+    accesstime <- getAccessTime filename
+    return [((takeFileName filename), (posixToUTC accesstime))]
+  return (concat alloutput)
+
+getAccessTimeFullPathR :: FilePath -> IO [(FilePath, POSIXTime)]
+getAccessTimeFullPathR topPath = do
+  allFiles <- listDirCompletePathR topPath
+  alloutput <- forM allFiles $ \filename -> do
+    accesstime <- getAccessTime filename
+    return [(filename, accesstime)]
+  return (concat alloutput)
 
 getChangeTime :: FilePath -> IO (POSIXTime)
 getChangeTime filePath =
   do stat <- getFileStatus filePath
      return (statusChangeTimeHiRes stat)
 
--- Wrapper function
+getModificationTime :: FilePath -> IO (POSIXTime)
+getModificationTime filePath =
+  do stat <- getFileStatus filePath
+     return (modificationTimeHiRes stat)
+
 posixToUTC :: POSIXTime -> UTCTime
 posixToUTC inputPosixTime = posixSecondsToUTCTime inputPosixTime
 
 main :: IO ()
 main = do
   (input:_) <- getArgs
-  accessTime <- getAccessTime input
-  modifTime <- getModificationTime input
-  changeTime <- getChangeTime input
-  putStrLn (show input ++ "\t Access: " ++ show (posixToUTC accessTime) ++ "\t Modify: " ++ show (posixToUTC modifTime) ++ "\t Change: " ++ show (posixToUTC changeTime))
+  otheraccesstime <- getAccessTimeR input
+  putStrLn (show input ++ ", " ++ show otheraccesstime)
