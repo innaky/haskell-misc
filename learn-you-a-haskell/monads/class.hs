047224fa8cdedd940a3cab05c5c6c828681cea4e
Innaky <innaky@protonmail.com>
2020-03-12 16:19:28 -0400

    About monads.

---
+++ learn-you-a-haskell/monads/class.hs
@@ -0,0 +1,13 @@
+class Monad m where
+  return :: a -> m a
+  (>>=) :: m a -> (a -> m b) -> m b
+  (>>) :: m a -> m b -> m b
+  x >> y = x >>= \_ -> y
+  fail :: String -> m a
+  fail msg -> error msg
+
+instance Monad Maybe where
+  return x = Just x
+  Nothing >>= f = Nothing
+  Just x >>= f = f x
+  fail _ = Nothing
