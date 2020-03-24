e051308138073569f6c639d3eaef8033ea7e43a7
Innaky <innaky@protonmail.com>
2020-03-24 12:06:24 -0400

    understanding instances and data types.

--- learn-you-a-haskell/type_and_typeclasses/datas_fn.hs
+++ learn-you-a-haskell/type_and_typeclasses/datas_fn.hs
@@ -2,7 +2,40 @@ data Nat = Zero | succ Nat
 data List a = Nil | Cons a (List a)
 data Tree a = Leaf a | Node (Tree a) a (Tree a)
 data Maybe a = Nothing | Just a
+data Some a b = Left a | Right b
+data Shape = Circle Float | Rec Float Float
 
+instance (Eq a) => Eq (Maybe a) where
+  Nothing == Nothing = True
+  Just x  == Just y  = x == y
+  _       == _       = False
+
+instance Eq Nat where
+  Zero == Zero = True
+  Nat  == Nat  = True
+  _    == _    = False
+
+instance Eq List where
+  Nil      == Nil    = True
+  Cons a b == Cons c d = (a == c) && (b == d) 
+  _        == _      = False
+
+instance Functor Maybe where
+  fmap _ Nothing  = Nothing
+  fmap f (Just a) =  (Just f a)
+
+instance Functor [] where
+  fmap f Nil = Nil
+  fmap f (Cons a b) = Cons (f a) (fmap Cons f b)
+
+instance Functor (Some a) where
+  fmap _ (Left a) = Left a
+  fmap f (Right a) = Right (f a)
+
+instance Functor Tree where
+  fmap f (Leaf a) = Leaf (f a)
+  fmap f (Node l a r) = Node (fmap f l) (f a) (fmap f r)
+  
 -- List
 len :: List a -> Int
 len Nil = 0
