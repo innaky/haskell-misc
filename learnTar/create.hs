d2d38dab9d6fb90b66d5de5469778cecf58d926c
Innaky <innaky@protonmail.com>
2020-03-31 15:21:13 -0400

    write :: [Entry] -> LBS.ByteString

--- learnTar/create.hs
+++ learnTar/create.hs
@@ -1,5 +1,12 @@
 module Create where
-import qualified Data.ByteString.Lazy as BS
+
+import qualified Data.ByteString      as BS
+import qualified Data.ByteString.Lazy as LBS
+
 create :: FilePath -> FilePath -> [FilePath] -> IO ()
 create tar base paths = BS.readFile tar . write =<< pack base paths
+
+write :: [Entry] -> LBS.ByteString
+write es = LBS.concat $ map putEntry es ++ [LBS.replicate (512*2) 0]
+
 
