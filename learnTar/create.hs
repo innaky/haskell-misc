7d0db527072da11213957758004cf52f18644c5d
Innaky <innaky@protonmail.com>
2020-03-31 15:42:03 -0400

    Adding data types data Entry, TarPath, EntryContent, Ownership, Format type FileSize, DevMajor, DevMinor, TypeCode, Permissions, EpochTime newtype LinkTarget

--- learnTar/create.hs
+++ learnTar/create.hs
@@ -2,7 +2,8 @@ module Create where
 
 import qualified Data.ByteString      as BS
 import qualified Data.ByteString.Lazy as LBS
-
+import Data.Int (Int64)
+import System.Posix.Type (FileMode)
 
 data Entry = Entry {
   entryTarPath :: {-# UNPACK #-} !TarPath,
@@ -12,7 +13,48 @@ data Entry = Entry {
   entryTime :: {-# UNPACK #-} !EpochTime,
   entryFormat :: !Format
   }
-  derivin (Eq, Show)
+  deriving (Eq, Show)
+
+data TarPath = TarPath {-# UNPACK #-} !BS.ByteString
+                       {-# UNPACK #-} !BS.ByteString
+  deriving (Eq, Ord)
+
+data EntryContent = NormalFile LBS.ByteString {-# UNPACK #-} !FileSize
+                    | Directory
+                    | SymbolicLink    !LinkTarget
+                    | HardLink        !LinkTarget
+                    | CharacterDevice {-# UNPACK #-} !DevMajor
+                                      {-# UNPACK #-} !DevMinor
+                    | BlockDevice     {-# UNPACK #-} !DevMajor
+                                      {-# UNPACK #-} !DevMinor
+                    | NamedPipe
+                    | OtherEntryType  {-# UNPACK #-} !TypeCode LBS.ByteString
+                                      {-# UNPACK #-} !FileSize
+  deriving (Eq, Ord, Show)
+
+data Ownership = Ownership {
+  ownerName :: String,
+  groupName :: String,
+  ownerId :: {-# UNPACK #-} !Int,
+  groupId :: {-# UNPACK #-} !Int
+  }
+  deriving (Eq, Ord, Show)
+
+data Format =
+  V7Format
+  | UstarFormat
+  | GnuFormat
+  deriving (Eq, Ord, Show)
+
+type FileSize = Int64
+type DevMajor = Int
+type DevMinor = Int
+type TypeCode = Char
+type Permissions = FileMode
+type EpochTime = Int64
+
+newtype LinkTarget = LinkTarget BS.ByteString
+ deriving (Eq, Ord, Show)
 
 create :: FilePath -> FilePath -> [FilePath] -> IO ()
 create tar base paths = BS.readFile tar . write =<< pack base paths
