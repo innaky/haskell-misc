874d4c72d4e7e98d87e7b49b588f3ce30f9c7403
Innaky <innaky@protonmail.com>
2020-04-03 03:46:38 -0400

    Adding more functions for create tar

--- learnTar/create.hs
+++ learnTar/create.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE CPP #-}
+
 module Create where
 
 import Data.Monoid (mempty)
@@ -5,15 +7,21 @@ import Numeric     (showOct)
 import Data.List   (foldl')
 import Data.Char   (ord)
 import System.FilePath ((</>))
-import System.Directory (doesDirectoryExist)
+import System.Directory ( doesDirectoryExist, getModificationTime, Permissions(..), getPermissions,
+                          getDirectoryContents )
 import System.Posix.Type (FileMode)
 import Data.Int (Int64)
+import System.IO ( IOMode(ReadMode), openBinaryFile, hFileSize )
+import System.IO.Unsafe (unsafeInterleaveIO)
 
 import qualified Data.ByteString       as BS
 import qualified Data.ByteString.Char8 as BS.Char8
 import qualified Data.ByteString.Lazy  as LBS
 import qualified Data.ByteString.Lazy  as LBS.Char8
-import qualified System.FilePath       as FilePath.Native ( addTrailingPathSeparator)
+import qualified System.FilePath       as FilePath.Native
+       ( addTrailingPathSeparator, hasTrailingPathSeparator, joinPath, splitDirectories)
+import qualified System.FilePath.Posix as FilePath.Posix
+       (splitPath, joinPath)
 
 data Entry = Entry {
   entryTarPath :: {-# UNPACK #-} !TarPath,
@@ -200,3 +208,125 @@ packPaths baseDir paths =
   | relpath <- paths
   , let isDir = FilePath.Native.hasTrailingPathSeparator filepath
         filepath = baseDir </> relpath ]
+
+interleave :: [IO a] -> IO [a]
+interleave = unsafeInterLeaveIO . go
+  where
+    go [] = return []
+    go (x:xs) = do
+      x' <- x
+      xs' <- interleave xs
+      return (x':xs')
+
+getDirectoryContentsRecursive :: FilePath -> IO [FilePath]
+getDirectoryContentsRecursive dir0 =
+  fmap tail (recurseDirectories dir0 [""])
+
+recurseDirectories :: FilePath -> [FilePath] -> IO [FilePath]
+recurseDirectories _ [] = return []
+recurseDirectories base (dir:dirs) = unsafeInterLeaveIO $ do
+  (files, dirs') <- collect [] [] =<< getDirectoryContents (base </> dir)
+
+  files' <- recurseDirectories base (dirs' ++ dirs)
+  return (dir : files ++ files')
+
+  where
+    collect files dirs' [] = return (reverse files, reverse dirs')
+    collect files dirs' (entry:entries) | ignore entry = collect files dirs' entries
+    collect files dirs' (entry:entries) = do
+      let dirEntry = dir </> entry
+          dirEntry' = FilePath.Native.addTrailingPathSeparator dirEntry
+      isDirectory <- doesDirectoryExist (base </> dirEntry)
+      if isDirectory
+        then collect files (dirEntry':dirs') entries
+        else collect (dirEntry:files) dirs' entries
+
+    ignore ['.']      = True
+    ignore ['.', '.'] = True
+    ignore _          = False
+
+toTarPath :: Bool -> FilePath -> Either String TarPath
+toTarPath isDir = splitLongPath . addTrailingSep . FilePath.Posix.joinPath
+                  . FilePath.Native.splitDirectories
+  where
+    addTrailingSep | isDir = FilePath.Posix.addTrailingPathSeparator
+                   | otherwise = id
+
+splitLongPath :: FilePath -> Either String TarPath
+splitLongPath path =
+  case packName nameMax (reverse (FilePath.Posix.splitPath path)) of
+    Left err                 -> Left err
+    Right (name, [])         -> Right $! TarPath (BS.Char8.pack name) BS.empty
+    Right (name, first:rest) -> case packName prefixMax remainder of
+      Left err               -> Left err
+      Right (_, (_:_))       -> Left "File name too long (cannot split)"
+      Right (prefix, [])     -> Right $! TarPath (BS.Char8.pack name) (BS.Char8.pack prefix)
+      where
+        remainder = init first : rest
+  where
+    nameMax, prefixMax :: Int
+    nameMax   = 100
+    prefixMax = 155
+    
+    packName _ [] = Left "File name empty"
+    packName maxLen (c:cs)
+      | n > maxLen = Left "File name too long"
+      | otherwise  = Right (packName' maxLen n [c] cs)
+      where n = length c
+      
+    packName' maxLen n ok (c:cs)
+      | n' <= maxLen    = packName' maxLen n' (c:ok) cs
+                            where n' = n + length c
+    packName' _ _ ok cs = (FilePath.Posix.joinPath ok, cs)
+
+
+packDirectoryEntry :: FilePath -> TarPath -> IO Entry
+packDirectoryEntry filepath tarpath = do
+  mtime <- getModTime filepath
+  return (directoryEntry tarpath) {
+    entryTime = mtime
+  }
+
+getModTime :: FilePath -> IO EpochTime
+getModTime path = do
+#if MIN_VERSION_directory(1,2,0)
+  t <- getModificationTime path
+  return . floor . utcTimeToPOSIXSeconds $ t
+#else
+  (TOD s _) <- getModificationTime path
+  return $! fromIntegral s
+#endif
+
+packFileEntry :: FilePath -> TarPath -> IO Entry
+packFileEntry filepath tarpath = do
+  mtime <- getModTime filepath
+  perms <- getPermissions filepath
+  file <- openBinaryFile filepath ReadMode
+  size <- hFileSize file
+  content <- LBS.hGetContents file
+  return (simpleEntry tarpath (NormalFile content (fromIntegral size))) {
+    entryPermissions = if executable perms then executableFilePermissions
+                                           else ordinaryFilePermissions,
+    entryTime = mtime
+  }
+
+simpleEntry :: TarPath -> EntryContent -> Entry
+simpleEntry tarpath content = Entry {
+  entryTarPath     = tarpath,
+  entryContent     = content,
+  entryPermissions = case content of
+                       Directory -> directoryPermissions
+                       _         -> ordinaryFilePermissions,
+  entryOwnership   = Ownership "" "" 0 0,
+  entryTime        = 0,
+  entryFormat      = UstarFormat
+  }
+
+directoryPermissions :: Permissions
+directoryPermissions = 0o0755
+
+ordinaryFilePermissions :: Permissions
+ordinaryFilePermissions = 0o0644
+
+executableFilePermissions :: Permissions
+executableFilePermissions = 0o0755
